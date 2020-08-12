e94ed472cae05d324891221884211f8ba22f5d32
szopen1228 <67273348+szopen1228@users.noreply.github.com>
2020-08-13 01:30:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -41,3 +41,31 @@ function opacity_logo2() {
     useMethods();
   }
 }
+
+
+
+
+
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}

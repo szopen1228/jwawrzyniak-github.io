f054853d6d335cf80e485611780e2e13552de2ed
szopen1228 <67273348+szopen1228@users.noreply.github.com>
2020-08-24 19:29:08 +0200

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>JAKUB WAWRZYNIAK</title>
+    <link rel="stylesheet" href="style.css" type="text/css" media="screen" title="no title">
+    <link rel="icon" href="images/logo_White.png">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+  </head>
+  <body>
+
+    <div class="navbar">
+      <a href="#home"><img class="logo" src="images/logo_white.png" alt="JW Logo"/ width="60px"></a>
+      <div class="navbar-buttons">
+        <span><img class="open-menu" src="images/menu.svg" alt=""/></span>
+      </div>
+    </div>
+
+    <div class="overlay">
+      <img id="close-menu" class="close-menu" src="images/close.png" width="30px" alt=""/>
+      <nav class="overlay-menu">
+        <ul>
+          <li><a class="close-menu" href="#home">GŁÓWNA</a></li>
+          <li><a class="close-menu" href="#about">O MNIE</a></li>
+          <li><a class="close-menu" href="#port">PORTFOLIO</a></li>
+          <li><a class="close-menu" href="#cont">KONTAKT</a></li>
+        </ul>
+      </nav>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.js" charset="utf-8"></script>
+    <script src="app.js" charset="utf-8"></script>
+
+
+    <div class="content">
+
+    <section class="home" id="home">
+    <video autoplay muted loop id="myVideo">
+  <source src="images/strona.mp4" type="video/mp4">
+  Your browser does not support HTML5 video.
+  </video>
+
+      <div class="home_text">
+        JAKUB WAWRZYNIAK</br>
+        <h1>GRAPHIC DESIGNER</h1>
+      </div>
+
+</section>
+<section class="about" id="about">
+  Oooo dupeczka
+</section>
+
+</div>
+
+  </body>
+</html>

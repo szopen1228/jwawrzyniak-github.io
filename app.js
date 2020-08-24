56dc03631177d8b5ee06dbf5450da7433d679cb9
szopen1228 <67273348+szopen1228@users.noreply.github.com>
2020-08-24 19:30:17 +0200

    Update and rename script.js to app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+$(document).ready(function close() {
+  $('.open-menu').on('click', function() {
+     $('.overlay').addClass('open');
+  });
+
+  $('.close-menu').on('click', function() {
+    $('.overlay').removeClass('open');
+  });
+});
